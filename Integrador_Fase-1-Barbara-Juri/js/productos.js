aeed93a44371cb2557dffe171fa092a25f11f65d
BarbaraLuzJ <135920967+BarbaraLuzJ@users.noreply.github.com>
2023-09-28 22:25:00 -0300

    Commit fase 1 integrador

---
+++ Integrador_Fase-1-Barbara-Juri/js/productos.js
@@ -0,0 +1,10 @@
+/* ---------------------------------------------------------- */
+/*                     Variables Globales                     */
+/* ---------------------------------------------------------- */
+var productos = [
+    { nombre: 'Pouch Royal Canin Intense Beauty', precio: 990, stock: 44, marca: 'Royal Canin', categoria: 'Gato', subcategoria: 'Alimentos', envio: false, foto: 'img-productos/Pouch-intenseBeauty.webp'},
+    { nombre: 'Bebedero', precio: 4550, stock: 30, marca: 'PetsPlast', categoria: 'Perro', subcategoria: 'Accesorios', envio: true, foto:'img-productos/Bebedero.webp'},
+    { nombre: 'Mix para loros', precio: 1250, stock: 100, marca: 'Nelsoni Ranch', categoria: 'Aves', subcategoria: 'Alimentos', envio: true, foto: 'img-productos/mix-loros.png' },
+    { nombre: 'Kit completo acuario deluxe', precio: 34567, stock: 23, marca: 'Marina', categoria: 'Peces', subcategoria: 'Accesorio', envio: false, foto: 'img-productos/kit-acuario.webp' },
+    { nombre: 'Hamstera con tobogan', precio: 17650, stock: 20, marca: 'Millex', categoria: 'Otras especies', subcategoria: 'Otros', envio: true, foto: 'img-productos/hamstera-tobogan.webp' },
+]
