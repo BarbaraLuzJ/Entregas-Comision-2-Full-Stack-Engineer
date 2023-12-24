1f0014cc8e97667bb4deb628a234dc4efa4be204
BarbaraLuzJ <135920967+BarbaraLuzJ@users.noreply.github.com>
2023-12-24 15:54:36 -0300

    Update config.js

--- Ejercicio_3-Barbara-Juri/config.js
+++ Ejercicio_3-Barbara-Juri/config.js
@@ -1,6 +1,6 @@
 const MODO_PERSISTENCIA = 'MONGODB' 
 const PORT = process.env.PORT || 8080 
-const STRCNX = 'mongodb+srv://barbaraljuri:<password>@proyectos.vs0v0qd.mongodb.net/?retryWrites=true&w=majority'
+const STRCNX = 'mongodb+srv://barbaraljuri:Molina85.@proyectos.vs0v0qd.mongodb.net/?retryWrites=true&w=majority'
 const BASE = 'Entregas'
 
 export default {
